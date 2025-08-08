acb0d6f8842a7127181f30e20b34cd71f0c0452b
MohamedEmbrator <embrator233@gmail.com>
2025-08-08 23:04:33 +0300

    edit type

--- src/app/admin/articles-table/edit/[id]/page.tsx
+++ src/app/admin/articles-table/edit/[id]/page.tsx
@@ -8,7 +8,7 @@ interface EditArticlePageProps {
     params: { id: string };
 }
 
-const EditArticlePage = async ({ params }: { params: { id: string }; }) => {
+const EditArticlePage = async ({ params }: any) => {
   const token = (await cookies()).get("jwtToken")?.value;
   if (!token) redirect("/");
   const payload = verifyTokenForPage(token);
